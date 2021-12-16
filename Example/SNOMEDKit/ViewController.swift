8e3a6070699f25fbcd4054c15f80879b60bf0f2a
Francis Li <mail@francisli.com>
2021-12-16 13:54:18 -0800

    Initial commit

---
+++ Example/SNOMEDKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SNOMEDKit
+//
+//  Created by Francis Li on 12/16/2021.
+//  Copyright (c) 2021 Francis Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
